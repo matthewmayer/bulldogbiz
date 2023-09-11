bdd074655edcea57d8216c8ce598c05d5240390d
Matt Mayer <matt@matthewmayer.co.uk>
2023-09-11 07:37:32 +0700

    image cache

--- .eleventy.js
+++ .eleventy.js
@@ -1,7 +1,26 @@
+"use strict"
+/* jshint node: true, esversion:11, asi:true */
+const Image = require("@11ty/eleventy-img");
 module.exports = function (eleventyConfig) {
     //copy anything in src/static into the root
     eleventyConfig.addPassthroughCopy({
         "src/static": "/"
+    });
+    eleventyConfig.addShortcode("image", async function (src, alt, sizes) {
+        let metadata = await Image(src, {
+            outputDir: "./_site/airtableimages",
+            urlPath: "/airtableimages/"
+        });
+
+        let imageAttributes = {
+            alt,
+            sizes,
+            loading: "lazy",
+            decoding: "async",
+        };
+
+        // You bet we throw an error on a missing alt (alt="" works okay)
+        return Image.generateHTML(metadata, imageAttributes);
     });
     return {
         dir: {
@@ -9,4 +28,5 @@ module.exports = function (eleventyConfig) {
             output: "_site"
         }
     };
+
 };
